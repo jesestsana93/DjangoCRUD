01452a3c522248e27478e8cb9798cd9ddffd8af7
Jesús E. Sánchez <jesestsana93@hotmail.com>
2019-08-22 12:43:15 -0500

    consultas

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,25 @@
 /*CONSULTAS PARA APLICACION MASCOTA CON DJANGO*/
 SELECT *
+FROM adopcion_persona;
+
+SELECT *
+FROM adopcion_solicitud;
+
+SELECT *
+FROM auth_permission;
+
+SELECT *
 FROM auth_user;
+
+SELECT *
+FROM django_session;
+
+SELECT *
+FROM mascota_mascota;
+
+SELECT *
+FROM mascota_mascota_vacuna;
+
+SELECT *
+FROM mascota_vacuna;
+
